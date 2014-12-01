3f7a7d4ed09c8246522e744033c825e497ca6a5b
jim <jim@rybarski.com>
2014-12-01 09:12:40 -0600

    more tests

--- tests/model/timestamp.py
+++ tests/model/timestamp.py
@@ -3,8 +3,18 @@ from fylm.model.timestamp import Timestamps
 
 
 class TimestampsTests(unittest.TestCase):
+    def setUp(self):
+        self.t = Timestamps()
+
     def test_parse_line(self):
-        t = Timestamps()
-        index, timestamp = t._parse_line("238 4.5356246")
+        index, timestamp = self.t._parse_line("238 4.5356246")
         self.assertEqual(index, 238)
         self.assertAlmostEqual(timestamp, 4.5356246)
+
+    def test_parse_line_invalid(self):
+        with self.assertRaises(AttributeError):
+            index, timestamp = self.t._parse_line("238")
+
+    def test_parse_line_empty(self):
+        with self.assertRaises(AttributeError):
+            index, timestamp = self.t._parse_line("")
