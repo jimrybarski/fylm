5f9aad29972d8e2287b36a38e4853a4f34f7de28
jim <jim@rybarski.com>
2014-12-04 13:22:05 -0600

    wrote init for image reader

---
+++ fylm/service/image_reader.py
@@ -0,0 +1,17 @@
+from nd2reader import Nd2
+
+
+class ImageReader(object):
+    """
+    Gets ImageSet objects from the nd2reader library, sets metadata as appropriate, applies transformations to each image
+    to correct for rotational and translational distortion, and yields the ImageSet. Seamlessly gets images from separate
+    ND2 files for an unlimited number of timepoints, so the application will behave as if there was only a single acquisition.
+
+    The registration and rotation can be optionally deactivated in case they misbehave, and the start point can also be set.
+
+    """
+    def __init__(self, register_images=True, rotate_images=True, start=1):
+        self._register_images = register_images
+        self._rotate_images = rotate_images
+        self._start = start
+
