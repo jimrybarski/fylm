35de19729c4ccdc125d0fcce49cbe13ed75786d9
Jim Rybarski <jim@rybarski.com>
2014-12-22 21:46:11 -0600

    added instructions for eric

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 from fylm.service.experiment import Experiment as ExperimentService
 from fylm.activity import Activity
 
+# The date of the experiment you want to quantify. This must match the ND2 files.
+experiment_date = "141111"
 
-experiment = ExperimentService().get_experiment("141111", "/home/jim/Desktop/experiments")
+# The directory where the ND2 files are location. A directory will be created within
+# this directory, named whatever the experiment date is. All the output will go there.
+nd2_dir = "/home/jim/Desktop/experiments"
+
+experiment = ExperimentService().get_experiment(experiment_date, nd2_dir)
 
 act = Activity(experiment)
 act.calculate_rotation_offset()
 act.extract_timestamps()
-# act.calculate_registration()
-# act.input_channel_locations()
+act.calculate_registration()
+act.input_channel_locations()
 act.create_kymographs()
-# act.fov_movie()
-# act.annotate_kymographs()
+act.annotate_kymographs()
