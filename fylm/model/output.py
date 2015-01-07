0cff4a3a39a7746471f9674505eaf7661f8f43ba
jim <jim@rybarski.com>
2015-01-07 15:34:26 -0600

    some tests for annotations that are needed in output

--- fylm/model/output.py
+++ fylm/model/output.py
@@ -28,13 +28,18 @@ class Output(BaseTextFile):
         self.field_of_view = None
         self.channel = None
         self._data = defaultdict(dict)
-        self.timestamp_set = None
+        self.timestamp_set = None  # TimestampSet
+        self.annotation = None  # ChannelAnnotationGroup
         self.fluorescence_data = None
+        self.time_periods = None
 
     @property
     def lines(self):
-        for time_period in self.
-        for timestamp in self.timestamp_set.get_data(self.field_of_view, )
+        for time_period in self.time_periods:
+            cell_lengths = self.annotation.get_cell_lengths(time_period)
+            # TODO: load fluorescence data here
+            for time_index, timestamp in self.timestamp_set.get_data(self.field_of_view, time_period):
+                yield "%s %s" % (timestamp, cell_lengths.get(time_index))
 
     def add_cell_length(self, time_index, length):
         if length < 10:
