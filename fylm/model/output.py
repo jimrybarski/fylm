dc0e07bee0bdce8b52e2a95307f62fe7c9c6f448
Jim Rybarski <jim@rybarski.com>
2015-03-26 10:29:29 -0500

    updated output to use new cell length method

--- fylm/model/output.py
+++ fylm/model/output.py
@@ -50,12 +50,10 @@ class Output(BaseTextFile):
     @property
     def lines(self):
         for time_period in self.time_periods:
-            if self.annotation:
-                cell_lengths = self.annotation.get_cell_lengths(time_period)
             # TODO: load fluorescence data here
             for time_index, timestamp in self.timestamp_set.get_data(self.field_of_view, time_period):
                 if self.annotation:
-                    length = cell_lengths.get(time_index)
+                    length = self.annotation.get_cell_lengths(time_period, time_index)
                 else:
                     length = None
                 yield "%s\t%s" % (timestamp, length if length is not None else "NaN")
